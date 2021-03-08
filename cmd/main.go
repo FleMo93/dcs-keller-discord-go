78da102d9548c077421a9b4cfb52177e6e7e7757
FleMo93 <florianhammer@hotmail.de>
2021-03-08 09:14:14 +0100

    Add config json

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	m "dcskellerdiscordgo"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 )
+
+type configJson struct {
+	Discord struct {
+		Token                 string `json:"token"`
+		Channel               string `json:"channel"`
+		ServerStatusMessageID string `json:"serverStatusMessageId"`
+	} `json:"discord"`
+	Dcs struct {
+		ServerName string `json:"serverName"`
+		Account    struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		} `json:"account"`
+	} `json:"dcs"`
+}
 
 func main() {
 	arg := os.Args
@@ -18,6 +35,27 @@ func main() {
 	serverName := ""
 	createMessage := false
 	verbose := false
+
+	fileBytes, err := ioutil.ReadFile("./config.json")
+
+	if err != nil && !os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	if err == nil {
+		config := configJson{}
+		err := json.Unmarshal(fileBytes, &config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		token = config.Discord.Token
+		botChannel = config.Discord.Channel
+		serverStatusMessageID = config.Discord.ServerStatusMessageID
+		username = config.Dcs.Account.Username
+		password = config.Dcs.Account.Password
+		serverName = config.Dcs.ServerName
+	}
 
 	for _, ele := range arg {
 		if strings.Index(ele, "--token ") == 0 {
